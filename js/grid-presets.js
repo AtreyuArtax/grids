99ac6ec345294ba6a6d5072c7d4a5bc56518aa38
AtreyuArtax <82472620+AtreyuArtax@users.noreply.github.com>
2025-06-25 14:09:49 -0400

    Clean up

--- js/grid-presets.js
+++ js/grid-presets.js
@@ -2,7 +2,7 @@
 
 export const gridPresets = {
     "standardMath": {
-        squareSizeInput: 20,
+        squareSizeInput: 15,
         xMin: 0, xMax: 10, xIncrement: 1, xLabelEvery: 1, xAxisLabel: "x", xLabelOnZero: false, xAxisLabelOnRight: false, xAxisLabelType: 'numbers',
         yMin: 0, yMax: 10, yIncrement: 1, yLabelEvery: 1, yAxisLabel: "y", yLabelOnZero: false, yAxisLabelOnTop: false,
         suppressZeroLabel: false, showAxisArrows: true,
@@ -12,7 +12,7 @@ export const gridPresets = {
         xMinRadians: null, xMaxRadians: null, radianStepMultiplier: null, xGridUnitsPerRadianStep: null
     },
     "negativeAndPositive": {
-        squareSizeInput: 20,
+        squareSizeInput: 15,
         xMin: -10, xMax: 10, xIncrement: 1, xLabelEvery: 2, xAxisLabel: "x", xLabelOnZero: true, xAxisLabelOnRight: true, xAxisLabelType: 'numbers',
         yMin: -10, yMax: 10, yIncrement: 1, yAxisLabel: "y", yLabelEvery: 2, yLabelOnZero: true, yAxisLabelOnTop: true,
         suppressZeroLabel: true, showAxisArrows: true,
@@ -22,7 +22,7 @@ export const gridPresets = {
         xMinRadians: null, xMaxRadians: null, radianStepMultiplier: null, xGridUnitsPerRadianStep: null
     },
     "trigGraph": {
-        squareSizeInput: 20,
+        squareSizeInput: 15,
         xMin: null, xMax: null, xIncrement: null, // Overridden by radian settings
         xLabelEvery: 1, xAxisLabel: "", xLabelOnZero: true, xAxisLabelOnRight: true, xAxisLabelType: 'radians',
         yMin: -3, yMax: 3, yIncrement: 0.5, yLabelEvery: 2, yAxisLabel: "", yLabelOnZero: true, yAxisLabelOnTop: true,
@@ -34,7 +34,7 @@ export const gridPresets = {
         xGridUnitsPerRadianStep: 6 // Each π/2 spans 3 minor grid units
     },
     "trigGraphDegrees": {
-        squareSizeInput: 20,
+        squareSizeInput: 15,
         xMin: 0, xMax: 360, xIncrement: 15, xLabelEvery: 3, xAxisLabel: "Angle (°)", xLabelOnZero: true, xAxisLabelOnRight: false, xAxisLabelType: 'degrees',
         yMin: -3, yMax: 3, yIncrement: 0.5, yLabelEvery: 2, yAxisLabel: "y", yLabelOnZero: true, yAxisLabelOnTop: true,
         suppressZeroLabel: false, showAxisArrows: true,
@@ -44,7 +44,7 @@ export const gridPresets = {
         xMinRadians: null, xMaxRadians: null, radianStepMultiplier: null, xGridUnitsPerRadianStep: null
     },
     "velocity": {
-        squareSizeInput: 20,
+        squareSizeInput: 15,
         xMin: 0, xMax: 15, xIncrement: 0.5, xLabelEvery: 0, xAxisLabel: "Time (s)", xLabelOnZero: false, xAxisLabelOnRight: false, xAxisLabelType: 'numbers',
         yMin: -10, yMax: 10, yIncrement: 1, yLabelEvery: 0, yAxisLabel: "Velocity (m/s [E])", yLabelOnZero: true, yAxisLabelOnTop: false,
         suppressZeroLabel: true, showAxisArrows: true,
@@ -54,7 +54,7 @@ export const gridPresets = {
         xMinRadians: null, xMaxRadians: null, radianStepMultiplier: null, xGridUnitsPerRadianStep: null
     },
     "position": {
-        squareSizeInput: 20,
+        squareSizeInput: 15,
         xMin: 0, xMax: 15, xIncrement: 0.5, xLabelEvery: 0, xAxisLabel: "Time (s)", xLabelOnZero: false, xAxisLabelOnRight: false, xAxisLabelType: 'numbers',
         yMin: -10, yMax: 10, yIncrement: 1, yLabelEvery: 0, yAxisLabel: "Position (m [E])", yLabelOnZero: true, yAxisLabelOnTop: false,
         suppressZeroLabel: true, showAxisArrows: true,
@@ -64,7 +64,7 @@ export const gridPresets = {
         xMinRadians: null, xMaxRadians: null, radianStepMultiplier: null, xGridUnitsPerRadianStep: null
     },
     "acceleration": {
-        squareSizeInput: 20,
+        squareSizeInput: 15,
         xMin: 0, xMax: 15, xIncrement: 0.5, xLabelEvery: 0, xAxisLabel: "Time (s)", xLabelOnZero: true, xAxisLabelOnRight: false, xAxisLabelType: 'numbers',
         yMin: -10, yMax: 10, yIncrement: 1, yLabelEvery: 0, yAxisLabel: "Acceleration (m/s^2 [E])", yLabelOnZero: true, yAxisLabelOnTop: false,
         suppressZeroLabel: true, showAxisArrows: true,
@@ -74,7 +74,7 @@ export const gridPresets = {
         xMinRadians: null, xMaxRadians: null, radianStepMultiplier: null, xGridUnitsPerRadianStep: null
     },
     "graphPaperLetter_1_4in": {
-        squareSizeInput: 20, // 1/4 inch at 96dpi is 24px
+        squareSizeInput: 15, // 1/4 inch at 96dpi is 24px
         xMin: 0, xMax: 34, xIncrement: 1, xLabelEvery: 0, xAxisLabel: "", xLabelOnZero: false, xAxisLabelOnRight: false, xAxisLabelType: 'numbers',
         yMin: 0, yMax: 43, yIncrement: 1, yLabelEvery: 0, yAxisLabel: "", yLabelOnZero: false, yAxisLabelOnTop: false,
         suppressZeroLabel: true, showAxisArrows: false,
@@ -84,7 +84,7 @@ export const gridPresets = {
         xMinRadians: null, xMaxRadians: null, radianStepMultiplier: null, xGridUnitsPerRadianStep: null
     },
     "graphPaperLetter_1_5in": {
-        squareSizeInput: 20, // 1/5 inch at 96dpi is 19.2px
+        squareSizeInput: 15, // 1/5 inch at 96dpi is 19.2px
         xMin: 0, xMax: 42, xIncrement: 1, xLabelEvery: 0, xAxisLabel: "", xLabelOnZero: false, xAxisLabelOnRight: false, xAxisLabelType: 'numbers',
         yMin: 0, yMax: 53, yIncrement: 1, yLabelEvery: 0, yAxisLabel: "", yLabelOnZero: false, yAxisLabelOnTop: false,
         suppressZeroLabel: true, showAxisArrows: false,
@@ -94,7 +94,7 @@ export const gridPresets = {
         xMinRadians: null, xMaxRadians: null, radianStepMultiplier: null, xGridUnitsPerRadianStep: null
     },
     "graphPaperLetter_1cm": {
-        squareSizeInput: 20, // 1cm ≈ 37.8px at 96dpi
+        squareSizeInput: 15, // 1cm ≈ 37.8px at 96dpi
         xMin: 0, xMax: 21, xIncrement: 1, xLabelEvery: 0, xAxisLabel: "", xLabelOnZero: false, xAxisLabelOnRight: false, xAxisLabelType: 'numbers',
         yMin: 0, yMax: 27, yIncrement: 1, yLabelEvery: 0, yAxisLabel: "", yLabelOnZero: false, yAxisLabelOnTop: false,
         suppressZeroLabel: true, showAxisArrows: false,
@@ -104,7 +104,7 @@ export const gridPresets = {
         xMinRadians: null, xMaxRadians: null, radianStepMultiplier: null, xGridUnitsPerRadianStep: null
     },
     "graphPaperLetter_5mm": {
-        squareSizeInput: 20, // 5mm ≈ 18.9px at 96dpi
+        squareSizeInput: 15, // 5mm ≈ 18.9px at 96dpi
         xMin: 0, xMax: 43, xIncrement: 1, xLabelEvery: 0, xAxisLabel: "", xLabelOnZero: false, xAxisLabelOnRight: false, xAxisLabelType: 'numbers',
         yMin: 0, yMax: 55, yIncrement: 1, yLabelEvery: 0, yAxisLabel: "", yLabelOnZero: false, yAxisLabelOnTop: false,
         suppressZeroLabel: true, showAxisArrows: false,
@@ -114,7 +114,7 @@ export const gridPresets = {
         xMinRadians: null, xMaxRadians: null, radianStepMultiplier: null, xGridUnitsPerRadianStep: null
     },
     "polar": {
-    squareSizeInput: 20,
+    squareSizeInput: 15,
     xMin: -10, xMax: 10, xIncrement: 1, xLabelEvery: 1, xAxisLabel: "x", xLabelOnZero: true, xAxisLabelOnRight: false, xAxisLabelType: 'numbers',
     yMin: -10, yMax: 10, yIncrement: 1, yLabelEvery: 1, yAxisLabel: "y", yLabelOnZero: true, yAxisLabelOnTop: false,
     suppressZeroLabel: false, showAxisArrows: false,
